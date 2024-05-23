dbe659262910ab4aead8984fb5c58aa09477de1b
niranjan-h <guru@nexenstial.com>
2024-05-23 15:41:55 +0530

    01_basics/02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //Treat all JS code as new version
+
+// alert (3+3) //This is node js not browser
+
+let name= "Guru"
+let age = 25
+let isLoggedIn = false
+let state;
+ 
+//number
+//string => ""
+//boolean => true/false
+//null => standalone vlaue
+//undefined => value not assigned yet
+//symbol => unique
+
+//objects
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
