9c8246bc83f5c0769d4ccbd7080fe4cada929bc3
niranjan-h <guru@nexenstial.com>
2024-05-23 17:04:52 +0530

    dataytpe summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, Undefined, Sumbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+
+const ifLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 348903249023904234092342n
+console.log(typeof outsideTemp);
+
+// Reference (Non Primitive)
+
+// Array, Objects, Fundtions
+
+const heros = ["value1", "value2", 'value3'];
+
+let myObj = {
+    name: "Guru",
+    age : 25,
+}
+
+// const myFunction = funtion(){
+//     console.log("Hello World!");
+// }
