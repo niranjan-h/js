04970df5555e87d4e41f660285956143e46e95e2
niranjan-h <guru@nexenstial.com>
2024-05-23 16:04:27 +0530

    conversion of datatypes

---
+++ 01_basics/03_coversionsOperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = "Test"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => True
+// 0 => False
+// "" => Flase
+// "Test" => True
+
+
+let soemNumber = 33
+let stringNumber = String (soemNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
