326424dd1c2bcd65f10bc9bc87610e244fc18db4
niranjan-h <guru@nexenstial.com>
2024-05-23 15:20:24 +0530

    js basics/01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=12345
+let accountEmail= "guru@nexenstial.com"
+var accountPassword = "12345"
+accountCity="Hubli"
+
+// accountID=2
+
+accountEmail= "niranjan.h@icloud.com"
+accountPassword="123456"
+accountCity="Hubballi"
+
+console.log(accountID);
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
