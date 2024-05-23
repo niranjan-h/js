609b9b37d0847d2272bb4053c2b25a323169c116
niranjan-h <guru@nexenstial.com>
2024-05-23 16:23:37 +0530

    coversions

--- 01_basics/03_coversionsOperations.js
+++ 01_basics/03_coversionsOperations.js
@@ -24,5 +24,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let soemNumber = 33
 let stringNumber = String (soemNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************************** Operations ***********************************
+
+let value = 3
+let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Guru"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+
+let gameCounter = 100
+let newCounter=gameCounter++
+console.log(gameCounter);
+console.log(newCounter);
